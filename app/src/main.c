d09d4706e515ff42ddd0df914f0a82f19c70ca09
Machine-Hum <ryan.cjw@gmail.com>
2021-10-05 16:52:23 -0700

    Imported the BT HR project.

--- app/src/main.c
+++ app/src/main.c
@@ -1,47 +1,83 @@
+/* main.c - Application main entry point */
+
 /*
- * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2015-2016 Intel Corporation
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <errno.h>
 #include <zephyr.h>
-// #include <drivers/sensor.h>
+#include <sys/printk.h>
 
-// #include "app_version.h"
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+#include <sys/byteorder.h>
 
-// #include <logging/log.h>
-// LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+static void start_scan(void);
+
+static struct bt_conn *default_conn;
+
+static struct bt_uuid_16 uuid = BT_UUID_INIT_16(0);
+static struct bt_gatt_discover_params discover_params;
+static struct bt_gatt_subscribe_params subscribe_params;
+
+static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
+			 struct net_buf_simple *ad)
+{
+	char dev[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(addr, dev, sizeof(dev));
+
+	printk("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
+	    dev, type, ad->len, rssi);
+}
+
+static void start_scan(void)
+{
+	int err;
+
+	/* Use active scanning and disable duplicate filtering to handle any
+	 * devices that might update their advertising data at runtime. */
+	struct bt_le_scan_param scan_param = {
+		.type       = BT_LE_SCAN_TYPE_ACTIVE,
+		.options    = BT_LE_SCAN_OPT_NONE,
+		.interval   = BT_GAP_SCAN_FAST_INTERVAL,
+		.window     = BT_GAP_SCAN_FAST_WINDOW,
+	};
+
+	err = bt_le_scan_start(&scan_param, device_found);
+	if (err) {
+		printk("Scanning failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Scanning successfully started\n");
+}
+
+static struct bt_conn_cb conn_callbacks = {
+	.connected = NULL,
+	.disconnected = NULL,
+};
 
 void main(void)
 {
-// 	int ret;
-// 	const struct device *sensor;
-// 
-// 	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
-// 
-// 	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-// 	if (!device_is_ready(sensor)) {
-// 		LOG_ERR("Sensor not ready");
-// 		return;
-// 	}
-// 
-// 	while (1) {
-// 		struct sensor_value val;
-// 
-// 		ret = sensor_sample_fetch(sensor);
-// 		if (ret < 0) {
-// 			LOG_ERR("Could not fetch sample (%d)", ret);
-// 			return;
-// 		}
-// 
-// 		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-// 		if (ret < 0) {
-// 			LOG_ERR("Could not get sample (%d)", ret);
-// 			return;
-// 		}
-// 
-// 		printk("Sensor value: %d\n", val.val1);
-// 
-// 		k_sleep(K_MSEC(1000));
-// 	}
-}
+	int err;
+	err = bt_enable(NULL);
 
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	bt_conn_cb_register(&conn_callbacks);
+
+	start_scan();
+}
