6ffb16c09d9bce2bec651743bb3c72b581481e45
Machine-Hum <ryan.cjw@gmail.com>
2021-10-05 18:20:01 -0700

    FS working.

--- app/src/main.c
+++ app/src/main.c
@@ -12,12 +12,20 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+#include <device.h>
+#include <storage/disk_access.h>
+#include <logging/log.h>
+#include <fs/fs.h>
+#include <ff.h>
+
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/conn.h>
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h>
 #include <sys/byteorder.h>
+
+#include <stdio.h>
 
 static void start_scan(void);
 
@@ -26,6 +34,21 @@ static struct bt_conn *default_conn;
 static struct bt_uuid_16 uuid = BT_UUID_INIT_16(0);
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_subscribe_params subscribe_params;
+
+LOG_MODULE_REGISTER(main);
+
+static FATFS fat_fs;
+/* mounting info */
+static struct fs_mount_t mp = {
+	.type = FS_FATFS,
+	.fs_data = &fat_fs,
+};
+
+/*
+*  Note the fatfs library is able to mount only strings inside _VOLUME_STRS
+*  in ffconf.h
+*/
+static const char *disk_mount_pt = "/SD:";
 
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad)
@@ -68,6 +91,65 @@ static struct bt_conn_cb conn_callbacks = {
 void main(void)
 {
 	int err;
+
+    /* raw disk i/o */
+	do {
+		static const char *disk_pdrv = "SD";
+		uint64_t memory_size_mb;
+		uint32_t block_count;
+		uint32_t block_size;
+
+		if (disk_access_init(disk_pdrv) != 0) {
+			LOG_ERR("Storage init ERROR!");
+			break;
+		}
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+			LOG_ERR("Unable to get sector count");
+			break;
+		}
+		LOG_INF("Block count %u", block_count);
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+			LOG_ERR("Unable to get sector size");
+			break;
+		}
+		printk("Sector size %u\n", block_size);
+
+		memory_size_mb = (uint64_t)block_count * block_size;
+		printk("Memory Size(MB) %u\n", (uint32_t)(memory_size_mb >> 20));
+	} while (0);
+
+    mp.mnt_point = disk_mount_pt;
+
+	int res = fs_mount(&mp);
+
+	if (res == FR_OK) {
+		printk("Disk mounted.\n");
+	} else {
+		printk("Error mounting disk.\n");
+        while(1){};
+	}
+
+    
+	struct fs_file_t filep;
+    char buf[128];
+	fs_file_t_init(&filep);
+    int f = fs_open(&filep, "/SD:/fname.txt", (FS_O_CREATE | FS_O_WRITE | FS_O_APPEND));
+    size_t len = sprintf(buf, "Hello World\n");
+    printk("sprintf(): %d\n", len);
+    res = fs_write(&filep, buf, len);
+    printk("fs_write(): %d\n", res);
+    res = fs_close(&filep);
+    printk("fs_close(): %d\n", res);
+	res = fs_unmount(&mp);
+    printk("fs_umount(): %d\n", res);
+    printk("Disk Unmounted\n");
+
+    while(1){};
+
 	err = bt_enable(NULL);
 
 	if (err) {
@@ -80,4 +162,43 @@ void main(void)
 	bt_conn_cb_register(&conn_callbacks);
 
 	start_scan();
+}
+
+static int lsdir(const char *path)
+{
+	int res;
+	struct fs_dir_t dirp;
+	static struct fs_dirent entry;
+
+	fs_dir_t_init(&dirp);
+
+	/* Verify fs_opendir() */
+	res = fs_opendir(&dirp, path);
+	if (res) {
+		printk("Error opening dir %s [%d]\n", path, res);
+		return res;
+	}
+
+	printk("\nListing dir %s ...\n", path);
+	for (;;) {
+		/* Verify fs_readdir() */
+		res = fs_readdir(&dirp, &entry);
+
+		/* entry.name[0] == 0 means end-of-dir */
+		if (res || entry.name[0] == 0) {
+			break;
+		}
+
+		if (entry.type == FS_DIR_ENTRY_DIR) {
+			printk("[DIR ] %s\n", entry.name);
+		} else {
+			printk("[FILE] %s (size = %zu)\n",
+				entry.name, entry.size);
+		}
+	}
+
+	/* Verify fs_closedir() */
+	fs_closedir(&dirp);
+
+	return res;
 }
